1f1b9ca6069100f2b562f688a7231af8a09e4ae2
Francisco Javier Jimenez Cohen <fjimenez-ext@ultratech-inc.com>
2024-08-15 23:39:31 -0300

    add : se agrega modulo de authenticacion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Título de tu API')
+    .setDescription('Descripción de tu API')
+    .setVersion('1.0')
+    .addTag('tags') // Puedes agregar etiquetas para organizar tus endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
+
